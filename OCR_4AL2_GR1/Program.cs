2f7b01fecee118c7104e24b578e6770e416360a0
GTouchet <guillaumetouchet921@gmail.com>
2022-02-09 18:33:26 +0100

    Initialize

---
+++ OCR_4AL2_GR1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OCR_4AL2_GR1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
