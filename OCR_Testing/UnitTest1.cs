a0ba72eb6cdc4674a2e7f5597003b933e09984f5
GTouchet <guillaumetouchet921@gmail.com>
2022-02-10 11:23:12 +0100

    Init testing

---
+++ OCR_Testing/UnitTest1.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace OCR_Testing
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            1.Should().Be(1);
+        }
+    }
+}
