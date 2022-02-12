b7ad94d63aca6ff1b9743f7e65270b8951415af3
GTouchet <guillaumetouchet921@gmail.com>
2022-02-12 19:45:56 +0100

    Entry status testing

--- OCR_Testing/ChecksumTests.cs
+++ OCR_Testing/ChecksumTests.cs
@@ -7,19 +7,19 @@ namespace OCR_Testing
     public class ChecksumTests
     {
         [Fact]
-        public void Checksum_457508000_ShouldBe_Valid()
+        public void Checksum_457508000_IsValid()
         {
             new Checksum("457508000").IsValid.Should().BeTrue();
         }
 
         [Fact]
-        public void Checksum_664371495_ShouldBe_Invalid()
+        public void Checksum_664371495_IsInvalid()
         {
             new Checksum("12?664371495").IsValid.Should().BeFalse();
         }
 
         [Fact]
-        public void Checksum_12X13678X_ShouldBe_Invalid()
+        public void Checksum_12X13678X_IsInvalid()
         {
             new Checksum("12?13678?").IsValid.Should().BeFalse();
         }
---
+++ OCR_Testing/EntryTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using OCR_4AL2_GR1.Application.Models;
+using OCR_4AL2_GR1.OcrConfigurations;
+using Xunit;
+
+namespace OCR_Testing
+{
+    public class EntryTests
+    {
+        private readonly IOcrConfiguration configuration;
+        private Entry entry;
+
+        public EntryTests()
+        {
+            this.configuration = new Ocr4x3Number();
+        }
+
+        [Fact]
+        public void DecryptedCode_StatusIsAuthorized()
+        {
+            string[] cryptedEntry = new string[4]
+            {
+                "    _  _  _  _  _  _  _  _ ",
+                "|_||_   ||_ | ||_|| || || |",
+                "  | _|  | _||_||_||_||_||_|",
+                "                           ",
+            };
+
+            this.entry = new Entry(this.configuration, cryptedEntry);
+
+            this.entry.Status.Should().Be("");
+        }
+
+        [Fact]
+        public void DecryptedCode_StatusIsErrored()
+        {
+            string[] cryptedEntry = new string[4]
+            {
+                " _  _     _  _        _  _ ",
+                "|_ |_ |_| _|  |  ||_||_||_ ",
+                "|_||_|  | _|  |  |  | _| _|",
+                "                           ",
+            };
+
+            this.entry = new Entry(this.configuration, cryptedEntry);
+
+            this.entry.Status.Should().Be("ERR");
+        }
+
+        [Fact]
+        public void DecryptedCode_StatusIsUnreadable()
+        {
+            string[] cryptedEntry = new string[4]
+            {
+                "    _  _     _  _  _  _  _ ",
+                "  | _|  |  | _||_   ||_|| |",
+                "  ||_  _|  | _||_|  ||_| _|",
+                "                           ",
+            };
+
+            this.entry = new Entry(this.configuration, cryptedEntry);
+
+            this.entry.Status.Should().Be("ILL");
+        }
+    }
+}
